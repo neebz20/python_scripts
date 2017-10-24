d4467d9a178bea6ab268cfd8845eba3666345c16
David Nebel <dnebel20@gmail,com>
2017-10-23 22:01:15 -0500

    cinema_3

--- cinema.py
+++ cinema.py
@@ -6,22 +6,29 @@ films = {
     "Ghost Busters": [12,5]
     }
 
+# Check for proper indentation
+
 while True:
     print("What would you like to watch?")
     choice = input("What film would you like to watch?: ").strip().title()
 
     if choice in films:
-        age = int(input("How old are you?: ").strip()) 
+        age = int(input("How old are you?: ").strip())
+        
             # check user age
+            
         if age >= films[choice][0]:
             # check enough seats
-
-        if films[choice][1]>0    
-            print("Enjoy the film!")
-            films[choice][1] = films[choice][1]-1
-        else:
-            print("You are too young to see that film")
-
             
+            num_seats = films[choice][1]
+
+            if num_seats > 0:    
+                print("Enjoy the film!")
+                films[choice][1] = films[choice][1]-1
+            
+            else:
+                print("Sorry, we are sold out!")
+        else:
+            print("You are too young to see that film") 
     else:
         print("We don't have that film...")
